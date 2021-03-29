48c636251c8ce1f88e9d023e92158ff3a8f34159
odiel139 <odiel139@gmail.com>
2021-03-29 21:32:49 +0300

    matala2

---
+++ text.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 29 19:18:55 2021
+
+@author: odiel
+"""
+
+def revword(word:str):
+    little_word=word.lower()
+    good_word=little_word[::-1]
+    return good_word
+
+
+def countword():
+    count=0
+    hendle=open(r'C:\Users\odiel\Desktop\limudim\python\matala2\text.txt')
+    line=0
+    for x in hendle:
+        x=x.rstrip()
+        if line==0:
+            word=x.lower()
+            count=count+1
+        if line>0:
+            for wordd in x.split():
+                sofi_word=revword(wordd)
+                if sofi_word==word:
+                    count=count+1
+        line=line+1            
+    return count 
